a99e651f52b88562394a7c8621ff52693186bfaf
Aleksander Gondek <gondekaleksander@hotmail.com>
2018-11-18 21:22:05 +0100

    Add unit tests example

--- src/tic_tac_toe/mod.rs
+++ src/tic_tac_toe/mod.rs
@@ -3,3 +3,14 @@ use std::string;
 pub fn get_greetings() -> String {
     return String::from("Hello from Rust MCTS Tic Tac Toe!");
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_should_return_greetings() {
+        let returned_value = get_greetings();
+        assert_eq!(returned_value, String::from("Hello from Rust MCTS Tic Tac Toe!"));
+    }
+}
