756afdfa8e4eb9258421fca57c4c406d8342404f
Aleksander Gondek <gondekaleksander@hotmail.com>
2018-11-21 23:06:40 +0100

    Introduce Tic Tac Toe Board Cell State struct

--- src/tic_tac_toe/mod.rs
+++ src/tic_tac_toe/mod.rs
@@ -1,5 +1,29 @@
 use std::string;
 
+pub enum BoardCellState {
+    Empty,
+    X,
+    O
+}
+
+impl std::fmt::Display for BoardCellState {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let cell_repr: String;
+        match self {
+            BoardCellState::Empty => {
+                cell_repr = String::from(" ")
+            },
+            BoardCellState::X => {
+                cell_repr = String::from("X")
+            },
+            BoardCellState::O => {
+                cell_repr = String::from("O")
+            }
+        }
+        write!(f, "{}", cell_repr)
+    }
+}
+
 pub fn get_greetings() -> String {
     return String::from("Hello from Rust MCTS Tic Tac Toe!");
 }
@@ -12,5 +36,16 @@ mod tests {
     fn it_should_return_greetings() {
         let returned_value = get_greetings();
         assert_eq!(returned_value, String::from("Hello from Rust MCTS Tic Tac Toe!"));
+    }
+
+    #[test]
+    fn it_board_cell_state_display_trait() {
+        let should_be_x = BoardCellState::X.to_string();
+        let should_be_o = BoardCellState::O.to_string();
+        let should_be_empty = BoardCellState::Empty.to_string();
+
+        assert_eq!(should_be_x, "X");
+        assert_eq!(should_be_o, "O");
+        assert_eq!(should_be_empty, " ");
     }
 }
