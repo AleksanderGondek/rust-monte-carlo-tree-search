da7be4af6b6fc8ca4f05d0e45e7ddcbc84a780e8
Aleksander Gondek <gondekaleksander@hotmail.com>
2018-11-26 19:19:30 +0100

    Include simple loop to display the game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 extern crate rust_mcts;
 
+use std::io;
+
 fn main() {
     let game_of_ttt = rust_mcts::tic_tac_toe::game::new();
-    println!("{}", game_of_ttt);
+    
+    loop {
+        println!("{}", game_of_ttt);
+
+        println!("Your move!");
+        let mut player_move = String::new();
+        io::stdin().read_line(&mut player_move).expect("Failed to read line");
+        
+        match player_move {
+            _ => {
+                println!("Exiting!");
+                break;
+            }
+        }
+    }
 }
